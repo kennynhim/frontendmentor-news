706c50471a7bca0a3f4935efd4d69ea54622fa71
Kenny Nhim <kenny@visimo.ai>
2024-07-09 16:57:24 -0400

    update

--- news/src/App.jsx
+++ news/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { Container } from "@mui/material";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import Header from "./components/Header";
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <p>Hello</p>;
+    return (
+        <Container sx={{ border: "1px solid red" }}>
+            <Header />
+        </Container>
+    );
 }
 
 export default App;
---
+++ news/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+
+export default function Header() {
+    return (
+        <Grid container>
+            <Grid item xs={8}>
+                W.
+            </Grid>
+            <Grid item xs={4}></Grid>
+        </Grid>
+    );
+}
