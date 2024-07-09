dbc292af714434fabdd03dc0fcaca3c48361cc9b
Kenny Nhim <kenny@visimo.ai>
2024-07-09 17:09:44 -0400

    update

--- news/src/components/Header.jsx
+++ news/src/components/Header.jsx
@@ -3,10 +3,23 @@ import { Grid } from "@mui/material";
 export default function Header() {
     return (
         <Grid container>
-            <Grid item xs={8}>
+            <Grid
+                item
+                xs={8}
+                sx={{
+                    fontWeight: "800",
+                    fontSize: "4rem",
+                }}
+            >
                 W.
             </Grid>
-            <Grid item xs={4}></Grid>
+            <Grid item xs={4} sx={{ alignContent: "center", textAlign: "end" }}>
+                <span>Home</span>
+                <span>New</span>
+                <span>Popular</span>
+                <span>Trending</span>
+                <span>Categories</span>
+            </Grid>
         </Grid>
     );
 }
